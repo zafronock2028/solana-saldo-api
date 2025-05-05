cd3ffacfd40451e09eb32019374be9d6d4b00115
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-05-05 06:31:15 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
-dotenv.config();
+const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
+bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("Tu CHAT_ID es:", chatId);
-  bot.sendMessage(chatId, `¡Hola! Tu CHAT_ID es: ${chatId}`);
+  bot.sendMessage(chatId, `Tu chat ID es: ${chatId}`);
 });
